56a4bd57fa54ae2089e86a02f3df06de594522e7
J4ME5s <77157850+J4ME5s@users.noreply.github.com>
2021-01-11 09:21:32 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+basic.show_string("Think of a number between 1 to 10")
+basic.show_string("Input your answer here")
+input.button_is_pressed(Button.A)
+basic.show_string("The answer was...")
+basic.show_number(randint(1, 10))
---
+++ main.ts
@@ -0,0 +1,5 @@
+basic.showString("Think of a number between 1 to 10")
+basic.showString("Input your answer here")
+input.buttonIsPressed(Button.A)
+basic.showString("The answer was...")
+basic.showNumber(randint(1, 10))
